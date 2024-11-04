ee671054677f8c03aea8b83a56387837b75c0c10
Drew <drew@oberweis.dev>
2024-11-04 17:12:06 -0600

    another docker script update

--- dockrun.cmd
+++ dockrun.cmd
@@ -1,4 +1,5 @@
 docker build --tag drewoberweis/ucpevbot .
+docker stop ucpevbot_testing
 docker rm ucpevbot_testing
 docker create --env-file .env --name ucpevbot_testing drewoberweis/ucpevbot
 docker start ucpevbot_testing
