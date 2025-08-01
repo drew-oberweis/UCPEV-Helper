f6250350a7da85b04a248d867d51e5cdeb8e0912
Drew Oberweis <drew@oberweis.dev>
2025-08-01 14:50:05 -0500

    fix my stupidity
    I don't know how i went so long without realizing the NaN's came from... info that wasnt filled out for that route. Added handling for that.

--- route.py
+++ route.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Route:
     def __init__(self):
@@ -24,59 +27,58 @@ class Route:
         else:
             self.end_msg = f"End Location: {self.end_location} ({self.end_pin})"
 
-        route_message = f"Route Name: {self.name}\n{self.start_msg}\n{self.end_msg}\nPOI: {self.notable_location}\nDistance: {self.est_distance} miles\nGAIA Link: {self.gaia_link}\n\nRoute Description: {self.route_description}\n\n{self.extra}"
+        notable_msg = ""
+        est_distance_msg = ""
+        gaia_link_msg = ""
+        extra_msg = ""
+
+        if self.notable_location == self.notable_location: #non NaN
+            notable_msg = f"\nNotable Location: {self.notable_location}"
+
+        if self.est_distance == self.est_distance: #non NaN
+            est_distance_msg = f"\n{self.est_distance} miles"
+
+        if self.gaia_link == self.gaia_link: #non NaN
+            gaia_link_msg = f"\nGAIA Link: {self.gaia_link}"
+
+        if self.extra == self.extra: #non NaN
+            extra_msg = f"\n{self.extra}"
+
+        route_message = f"Route Name: {self.name}\n{self.start_msg}\n{self.end_msg}{notable_msg}{est_distance_msg}{gaia_link_msg}\n\nRoute Description: {self.route_description}\n\n{extra_msg}"
 
         return route_message
 
-    # decorator function to verify that none of the inputs are NaN or empty
-    def check_nan(func):
-        def wrapper(self, *args, **kwargs):
-            for arg in args:
-                if pd.isna(arg) or arg == "" or arg != arg:
-                    raise ValueError("Input cannot be NaN or empty")
-            return func(self, *args, **kwargs)
-        return wrapper
-
     #TODO: Add verifiers for relevant fields here
-    @check_nan
     def set_name(self, name: str):
         self.name = name
-
-    @check_nan
+        logger.debug(f"Route name set to: {self.name}")
     def set_start_location(self, start_location: str):
         self.start_location = start_location
-
-    @check_nan
+        logger.debug(f"Start location set to: {self.start_location}")
     def set_start_pin(self, start_pin: str):
         self.start_pin = start_pin
-
-    @check_nan
+        logger.debug(f"Start pin set to: {self.start_pin}")
     def set_notable_location(self, notable_location: str):
         self.notable_location = notable_location
-
-    @check_nan
+        logger.debug(f"Notable location set to: {self.notable_location}")
     def set_end_location(self, end_location: str):
         self.end_location = end_location
-
-    @check_nan
+        logger.debug(f"End location set to: {self.end_location}")
     def set_end_pin(self, end_pin: str):
         self.end_pin = end_pin
-
-    @check_nan
+        logger.debug(f"End pin set to: {self.end_pin}")
     def set_est_distance(self, est_distance: str):
         self.est_distance = est_distance
-
-    @check_nan
+        logger.debug(f"Estimated distance set to: {self.est_distance}")
     def set_gaia_link(self, gaia_link: str):
         self.gaia_link = gaia_link
-
-    @check_nan
+        logger.debug(f"GAIA link set to: {self.gaia_link}")
     def set_route_description(self, route_description: str):
         self.route_description = route_description
-
-    @check_nan
+        logger.debug(f"Route description set to: {self.route_description}")
     def set_extra(self, extra: str):
         self.extra = extra
+        logger.debug(f"Extra information set to: {self.extra}")
 
 
 def get_routes_from_df(routes_df: pd.DataFrame) -> list[Route]:
